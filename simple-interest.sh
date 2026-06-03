e9e4397d0430bbaab104e2370594c06a2f25f849
hashim.naim <work.hashimnaim@gmail.com>
2026-06-03 11:36:11 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+echo "Enter the Principal amount: "
+read principal
+
+echo "Enter the Rate of Interest (annual %): "
+read roi
+
+echo "Enter the Time period (in years): "
+read time
+
+interest=$(echo "scale=2; ($principal * $roi * $time) / 100" | bc -l)
+
+total_amount=$(echo "scale=2; $principal + $interest" | bc -l)
+
+echo "-------------------------------------"
+echo "Principal Amount : $principal"
+echo "Interest Earned  : $interest"
+echo "Total Amount     : $total_amount"
+echo "-------------------------------------"
 
